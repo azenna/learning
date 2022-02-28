81737a632b84bd60a4d5d2cefb96f7268841577f
ZacharyAllwein <zrallwein@gmail.com>
2022-02-28 12:39:44 -0600

    changed

--- rust/general/src/main.rs
+++ rust/general/src/main.rs
@@ -1,24 +1,9 @@
 fn main() {
-    let person = init_person("zach", 16);
 
     let mut rect = Rectangle::new(10, 10);
     rect.scale(10);
 
     println!("{:?} has an area of {}", rect, rect.area());
-}
-
-
-fn half_of_string(s: &String) -> &str{
-    &s[0..s.len()/2]
-}
-
-struct Person {
-    name: String,
-    age: u64,
-}
-
-fn init_person(name: &str, age: u64) -> Person{
-    Person{name: String::from(name), age: age,}
 }
 
 #[derive(Debug)]
