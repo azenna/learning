06f2bd4e5c2f99d37536de3f89effe5997a503eb
ZacharyAllwein <zrallwein@gmail.com>
2022-03-04 12:49:55 -0600

    working at last

--- Rust/general/src/main.rs
+++ Rust/general/src/main.rs
@@ -2,14 +2,10 @@ mod rectangle;
 mod ip_address;
 mod fibonacci_match;
 mod linked_list;
+mod hash_map;
 
 fn main() {
 
-    let mut head = linked_list::Node {data: 30, next: None};
-    head.append(5);
-    head.append(10);
-    head.append(15);
-    head.append(20);
-    head.print_values();
+    hash_map::hash_maps();
 
 }
