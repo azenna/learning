e1cb3c883e45880601f3336541e3b81e61e1a820
ZacharyAllwein <zrallwein@gmail.com>
2022-03-09 12:50:43 -0600

    precal test time :(

--- Rust/general/src/chapter_ten.rs
+++ Rust/general/src/chapter_ten.rs
@@ -1,8 +1,22 @@
 pub fn ten_main(){
 
-    let p = Point {x: "hello", y: "hello"};
+    let vid = Video {creator: String::from("john"),
+                        video: String::from("a;lsdjk;f;kasdfk;"),
+                        description: String::from("programming video"),
+                        title: String::from("I learn rust")
+                    };
 
-    println!("{}", p.concat());
+    let news_art = NewsArticle {
+        headline: String::from("fire while learning rust!"),
+        body: String::from("there was a fire while john was learning rust in the school library"),
+        author: String::from("john"),
+        date: String::from("3/9/22"),
+    };
+
+
+    push_notification(&vid);
+    push_notification(&news_art);
+    
 }
 
 //extract largest number functionality and put into a function
@@ -39,4 +53,42 @@ impl Point<&str, &str>{
     fn concat(&self) -> String{
         String::from(self.x) + self.y
     }
+}
+
+trait Summary {
+    fn summarize(&self) -> String{
+        let (author, main) = self.get_summary_content();
+        format!("From: {}, {}", author, main)
+    }
+    fn get_summary_content(&self) -> (&String, &String);
+}
+
+struct NewsArticle {
+    headline: String,
+    body: String,
+    author: String,
+    date: String,
+}
+
+impl Summary for NewsArticle {
+    fn get_summary_content(&self) -> (&String, &String){
+        return (&self.author, &self.headline);
+    }
+}
+
+struct Video {
+    title: String,
+    video: String,
+    description: String,
+    creator: String,
+}
+
+impl Summary for Video {
+    fn get_summary_content(&self) -> (&String, &String){
+        return (&self.creator, &self.title);
+    }
+}
+
+fn push_notification(item: &impl Summary){
+    println!("one new notification: {}", item.summarize());
 }
