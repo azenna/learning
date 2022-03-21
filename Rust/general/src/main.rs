95eaf25282904bb76d91e2b99004f910c2541042
ZacharyAllwein <zrallwein@gmail.com>
2022-03-21 12:49:28 -0500

    rust smart pointers are hurting my brain

--- Rust/general/src/main.rs
+++ Rust/general/src/main.rs
@@ -1,3 +1,14 @@
-mod iterators;
+mod smart_pointers;
+use smart_pointers::List::*;
+use std::rc::Rc;
 
-fn main() {}
+fn main() {
+    
+    let list_a = Rc::new(Cons(1, Rc::new(Cons(1, Rc::new(Cons(1, Rc::new(Cons(1, Rc::new(Nil)))))))));
+    let list_b = Cons(2, Rc::clone(&list_a));
+    {
+        let list_c = Cons(3, Rc::clone(&list_a));
+        println!("Rc count a: {}", Rc::strong_count(&list_a));
+    }
+    println!("Rc count a: {}", Rc::strong_count(&list_a));
+}
