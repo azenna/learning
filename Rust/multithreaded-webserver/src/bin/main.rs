78ef46dcc0f2a147ffa201a2e6f7a7658bbec04e
Zachary Allwein <zrallwein@gmail.com>
2022-03-27 01:02:26 -0500

    and on that note... he closed the rust book.. never to... wait! what's this he's opened the book back up! oh! he's forgotten how to create a vector! send help!

--- Rust/multithreaded-webserver/src/bin/main.rs
+++ Rust/multithreaded-webserver/src/bin/main.rs
@@ -3,18 +3,22 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
         pool.execute(|| {
             handle_connection(stream);
         })
     }
+
+    println!("Shutting down");
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -22,8 +26,12 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, path) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(10));
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
         ("HTTP/1.1 400 NOT FOUND", "404.html")
--- Rust/multithreaded-webserver/src/lib.rs
+++ Rust/multithreaded-webserver/src/lib.rs
@@ -5,9 +5,14 @@ use std::thread;
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
+enum Assignment {
+    Quit,
+    NewJob(Job),
+}
+
 pub struct ThreadPool {
     threads: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpsc::Sender<Assignment>,
 }
 
 impl ThreadPool {
@@ -30,24 +35,48 @@ impl ThreadPool {
         F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        self.sender.send(Assignment::NewJob(job)).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        self.threads
+            .iter()
+            .for_each(|_| self.sender.send(Assignment::Quit).unwrap());
+
+        for worker in &mut self.threads {
+            println!("Shutting down worker: {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
     }
 }
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Assignment>>>) -> Worker {
         Worker {
             id,
-            thread: thread::spawn(move || loop {
+            thread: Some(thread::spawn(move || loop {
                 let job = receiver.lock().unwrap().recv().unwrap();
-                println!("worker {} got a job", id);
-                job();
-            }),
+                match job {
+                    Assignment::NewJob(j) => {
+                        println!("Worker {} got a job", id);
+                        j();
+                    }
+                    Assignment::Quit => {
+                        println!("Exiting thread {}", id);
+                        break;
+                    }
+                }
+            })),
         }
     }
 }
