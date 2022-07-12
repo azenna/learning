0b07850d5f555eb1160ad90cc05f7445f937cedf
Zachary Allwein <zrallwein@gmail.com>
2022-07-12 17:30:58 -0500

    end of day

--- scheme/chapter_one.scm
+++ scheme/chapter_one.scm
@@ -126,4 +126,142 @@
         ((not (= 0 (remainder b 2))) (+ a (multiply a (- b 1)))) 
 	(else (multiply (* a 2) (/ b 2)))))
 
-        
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+
+(define (fib-iter a b p q count)
+  (cond ((= count 0) b)
+        ((= 0 (remainder count 2))
+         (fib-iter a
+                   b
+                   (+ (* p p) (* q q))
+                   (+ (* 2 q p) (* q q))
+                   (/ count 2)))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b p) (* a q))
+                        p
+                        q
+                        (- count 1)))))        
+
+(define (greatest_common_divisor a b)
+  (if (= 0 b)
+      a
+      (greatest_common_divisor b (remainder a b))))
+
+(define (next x)
+  (if (= x 2) 3 (+ x 2)))
+
+(define (smallest_divisor n)
+  (find_divisor n 2))
+
+(define (find_divisor n test)
+  ( cond ((= (remainder n test) 0) test)
+         ((> (* test test) n) n)
+         (else (find_divisor n (next test)))))
+
+(define (is_prime n)
+	(= n (smallest_divisor n)))
+
+(define (expmod n e m)
+  (cond ((= e 0) 1)
+        (else
+         (remainder (* n (expmod n (- e 1) m)) m))))
+     
+(define (fermat_test n)
+  (if (= (expmod (- n 1) n n) (- n 1)) 
+      #t
+      #f))
+
+(define (timed_prime_test n)
+  (newline)
+  (display n)
+  (start_prime_test n (runtime)))
+
+(define (start_prime_test n start_time)
+  (if (is_prime n)
+      (report_prime (- (runtime) start_time))
+      #f))
+
+(define (report_prime elapsed_time)
+  (display " *** ")
+  (display elapsed_time)
+  #t)
+
+(define (search_for_primes start count)
+  (cond ((= count 3))
+        ((is_prie start) (search_for_primes (+ start 1) (+ count 1)))
+        (else (search_for_primes (+ start 1) count))))
+
+(define (miller-rabin-test n)
+  (define (random n) (random-integer n))
+  (define (expmod base exp m)
+    (define (square-mod x) (remainder (* x x) m))
+    (define (square-signal-root x)
+     (display x)
+     (newline)
+     (if (and 
+	  (not (or (= 1 x) (= x (- m 1))))
+	  (= 1 (square-mod x)))
+      0
+      (square-mod x)))
+    (cond ((= exp 0) 1)
+     ((even? exp) (square-signal-root (expmod base (/ exp 2) m)))
+     (else
+      (remainder (* base (expmod base (- exp 1) m))
+                 m))))  
+  (define (try-it a)
+   (= (expmod a (- n 1) n) 1))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) #t)
+        ((miller-rabin-test n) (fast-prime? n (- times 1)))
+	(else #f)))
+
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a) (sum term (next a) next b))))
+
+(define (cube x) (* x x x))
+
+(define (sum_cubes a b) (sum cube a inc b))
+
+(define (inc x) (+ x 1))
+
+(define (sum_ints a b) (sum + a inc b))
+
+(define (pi_sum a b)
+  (define (pi_term x)
+    (/ 1.0 (* x (+ x 2))))
+  (define (pi_next x) (+ x 4))
+  (sum pi_term a pi_next b))
+
+(define (pi_approx d)
+  (* 8 (pi_sum 1 d))) 
+
+(define (integral f a b dx)
+  (define (add-dx x) (+ x dx))
+  (* (sum f (+ a (/ dx 2.0)) add-dx b) dx))
+
+(define (simpsons f a b n)
+  (define h (/ (- b a) n))
+  (define (add_two_h x) (+ x h h))
+
+  (* (/ h  3) (+ (f a) (f b) (* 2 (sum f a add_two_h b)) (* 4 ( sum f (+ a h) add_two_h b)))))
+
+(define (sum_iter term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result 
+        (iter (next a) (+ result (term a)))))
+   (iter a 0))
+
+(define (sum_iter_ints a b) (sum_iter + a inc b))
+
+(define (product a b term next)
+  (if (> a b)
+      1
+      (* (term a) (product (next a) b term next))))
+
+(define (factorial b) (product 1 b + (lambda (x) (+ x 1)))) 
