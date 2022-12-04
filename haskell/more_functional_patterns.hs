3c33e0c22fe74a9b2ca1671a0e5e189cb8ec92a5
Zachary Allwein <zrallwein@gmail.com>
2022-12-03 19:31:25 -0600

    just getting rid of starship angry red signs

--- haskell/more_functional_patterns.hs
+++ haskell/more_functional_patterns.hs
@@ -7,3 +7,22 @@ addOneIfOdd n = case odd n of
 addFive = \x -> \y -> min x y + 5
 
 mflip f x y = f y x
+
+f :: (a, b, c) -> (d, e, f) -> ((a, d), (c, f))
+f (a, _, c) (d, _, f) = ((a, d), (c, f))
+
+functionC x y =
+    case x > y of
+        True -> x
+        False -> y
+
+ifEvenAdd2 n = 
+    case even n of
+        True -> n + 2
+        False -> n
+
+nums x =
+    case compare x 0 of
+        LT -> -1
+        GT -> 1
+        EQ -> 0
