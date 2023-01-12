7cfc1880c205b48122fa0dd3935a8a6409993dbf
Zachary Allwein <zrallwein@gmail.com>
2023-01-12 10:05:57 -0600

    done with traversables

--- haskell/traverseable.hs
+++ haskell/traverseable.hs
@@ -2,6 +2,7 @@ import Test.QuickCheck
 import Test.QuickCheck.Checkers
 import Test.QuickCheck.Classes
 import Data.Monoid
+import Control.Applicative
 
 newtype Identity a = Identity a
   deriving (Eq, Ord, Show)
@@ -115,3 +116,112 @@ instance Foldable List where
   foldr _ i Nil = i
   foldr f i (Cons a l) = f a $ foldr f i l
 
+instance Traversable List where
+  traverse :: Applicative f =>
+              (a -> f b) -> List a -> f (List b)
+  traverse _ Nil = pure Nil
+  traverse f (Cons a l) = liftA2 Cons (f a) (traverse f l)
+
+triggerList :: 
+  List (Maybe Int, Maybe String, Integer, Any)
+triggerList = undefined
+
+testList = quickBatch $ traversable triggerList
+
+data Bigger a b = Bigger a b b b
+  deriving (Eq, Show)
+
+instance (Eq a, Eq b) => EqProp (Bigger a b) where
+  (=-=) = eq
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Bigger a b) where
+  arbitrary = 
+    liftA3 Bigger arbitrary arbitrary arbitrary <*> arbitrary
+
+instance Functor (Bigger a) where
+  fmap f (Bigger a b c d) = Bigger a (f b) (f c) (f d)
+
+instance Foldable (Bigger a) where
+  foldMap f (Bigger _ a b c) = f a <> f b <> f c
+
+instance Traversable (Bigger a) where
+  traverse f (Bigger a b c d) = liftA3 (Bigger a) (f b) (f c) (f d)
+
+triggerBigger :: 
+  Bigger (Maybe Int, Maybe String, Integer, Any)
+         (Maybe Int, Maybe String, Integer, Any)
+triggerBigger = undefined
+
+testBigger = quickBatch $ traversable triggerBigger
+
+{-# LANGUAGE FlexibleContexts #-}
+
+data S n a = S (n a) a deriving (Eq, Show)
+
+instance (Functor n,
+          Arbitrary (n a),
+          Arbitrary a) =>
+          Arbitrary (S n a) where
+  arbitrary = liftA2 S arbitrary arbitrary
+
+instance (Applicative n,
+          Testable (n Property),
+          EqProp a) =>
+          EqProp (S n a) where
+  (S x y) =-= (S p q) =
+        (property $ (=-=) <$> x <*> p)
+    .&. (y =-= q)
+
+instance Functor n => Functor (S n) where
+  fmap f (S a b) = S (f <$> a) (f b)
+
+instance Foldable n => Foldable (S n) where
+  foldMap f (S a b) = foldMap f a <> f b
+
+instance Traversable n => Traversable (S n) where
+  traverse f (S a b) = liftA2 S (traverse f a) (f b)
+
+sTrigger :: S [] (Maybe Int, Maybe String, Integer, Any)
+sTrigger = undefined
+
+sTest = quickBatch $ traversable sTrigger
+
+data Tree a =
+    Empty
+  | Leaf a
+  | Node (Tree a) a (Tree a)
+  deriving (Eq, Show)
+
+instance Eq a => EqProp (Tree a) where
+  (=-=) = eq
+
+instance Arbitrary a => Arbitrary (Tree a) where
+  arbitrary =
+    frequency [(1, return Empty),
+               (1, Leaf <$> arbitrary),
+               (1, liftA3 Node arbitrary
+                               arbitrary
+                               arbitrary)]
+
+--this was suprisingly easy
+--probably could write a HOF that generalizes this patter of tree traversal
+instance Functor Tree where
+  fmap _ Empty = Empty
+  fmap f (Leaf a) = Leaf $ f a
+  fmap f (Node l a r) = Node (fmap f l) (f a) (fmap f r)
+
+instance Foldable Tree where
+  foldMap _ Empty = mempty
+  foldMap f (Leaf a) = f a
+  foldMap f (Node l a r) = (foldMap f l) <> f a <> (foldMap f r)
+
+instance Traversable Tree where
+  traverse _ Empty = pure Empty
+  traverse f (Leaf a) = Leaf <$> f a
+  traverse f (Node l a r) = 
+    liftA3 Node (traverse f l) (f a) (traverse f r)
+
+treeTrigger :: Tree (Maybe Int, Maybe String, Integer, Any)
+treeTrigger = undefined
+
+treeTest = quickBatch $ traversable treeTrigger
