8f7ad77a7b3294ad26ab036f4859d97bef139441
Zachary Allwein <zrallwein@gmail.com>
2023-01-13 11:34:48 -0600

    done with readers, but probably not really, they seem important

--- haskell/reader.hs
+++ haskell/reader.hs
@@ -1,5 +1,7 @@
 import Control.Applicative
 import Data.Char
+import Data.Maybe
+import Data.Monoid
 
 boop :: Num a => a -> a
 boop = (*2)
@@ -82,4 +84,45 @@ instance Monad (Reader r) where
   (>>=) :: Reader r a -> (a -> Reader r b) -> Reader r b
   (>>=) (Reader f) g = Reader $ \r -> runReader (g (f r)) r
   
+x = [1, 2, 3]
+y = [4, 5, 6]
+z = [7, 8, 9]
+
+xs :: Maybe Integer
+xs = lookup 3 $ zip x y
+
+ys :: Maybe Integer
+ys = lookup 6 $ zip y z
+
+zs :: Maybe Integer
+zs = lookup 4 $ zip x y
+
+z' :: Integer -> Maybe Integer
+z' = (flip lookup) (zip x z)
+
+x1 :: Maybe (Integer, Integer)
+x1 = liftA2 (,) xs ys
+
+x2 :: Maybe (Integer, Integer)
+x2 = liftA2 (,) ys zs
+
+x3 :: Integer -> (Maybe Integer, Maybe Integer)
+x3 = liftA2 (,) z' z'
+
+summed :: Num c => (c, c) -> c
+summed = uncurry (+)
+
+bolt :: Integer -> Bool
+bolt = liftA2 (&&) (>3) (<8)
+
+sequA :: Integral a => a -> [Bool]
+sequA = sequenceA [(>3), (<8), even]
+
+s' = summed <$> x1
+
+main :: IO ()
+main = do
+  print $ getAll $ foldMap All $ sequA 7
+  print $ fromMaybe [] $ sequA <$> s'
+  print $ fromMaybe False $ bolt <$> ys
 
