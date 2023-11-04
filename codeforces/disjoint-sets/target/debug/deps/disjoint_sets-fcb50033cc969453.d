45b30d40861670133bd40334920fec13f9e97daf
Zenna Allwein <zrallwein@gmail.com>
2023-11-03 22:10:17 -0500

    uncommit target

--- codeforces/disjoint-sets/target/debug/deps/disjoint_sets-fcb50033cc969453.d
+++
@@ -1,5 +0,0 @@
-/home/zenna/software-engineering/learning/codeforces/disjoint-sets/target/debug/deps/disjoint_sets-fcb50033cc969453: src/main.rs
-
-/home/zenna/software-engineering/learning/codeforces/disjoint-sets/target/debug/deps/disjoint_sets-fcb50033cc969453.d: src/main.rs
-
-src/main.rs:
--- codeforces/disjoint-sets/target/debug/disjoint-sets.d
+++
@@ -1,1 +0,0 @@
-/home/zenna/software-engineering/learning/codeforces/disjoint-sets/target/debug/disjoint-sets: /home/zenna/software-engineering/learning/codeforces/disjoint-sets/src/main.rs
