63bd4a092d7608b2ba7683272f1a0a0a4de8da7b
azenna <zrallwein@gmail.com>
2024-03-07 01:07:54 -0600

    some cs

--- parallel_concurrent_haskell/flake.nix
+++ parallel_concurrent_haskell/flake.nix
@@ -4,14 +4,18 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { flake-utils, nixpkgs, self }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = [
-            (pkgs.haskellPackages.ghcWithPackages (pkgs: [ pkgs.parallel ]))
-          ];
-        };
-      });
+  outputs = {
+    flake-utils,
+    nixpkgs,
+    self,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = [
+          (pkgs.haskellPackages.ghcWithPackages (pkgs: [pkgs.parallel]))
+        ];
+      };
+    });
 }
